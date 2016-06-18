8f32533011986805c5a47780b64c6b7f99f7c192
glundgren93 <glundgren93@gmail.com>
2016-06-18 14:14:58 -0300

    refactoring profile component

--- src/components/Profile/presenter.js
+++ src/components/Profile/presenter.js
@@ -3,18 +3,28 @@ import { Link } from 'react-router';
 
 const Profile = ({ profile = {} }) => {
   return (
-      <div className="col-md-4 pull left">
+      <div className="col-md-4">
         <div class="row">
           <div className="col-md-9">
             <div className="thumbnail">
               <img src={ profile.avatar_url }></img>
               <div className="caption">
                 <h3>{ profile.name }</h3>
-                <p>{ profile.login }</p>
+                <h4>{ profile.login }</h4>
                 <hr />
                 <p>{ profile.location }</p>
-                <p>{ profile.email }</p>
-                <p>{ profile.blog }</p>
+                <p><a href='#'> { profile.email } </a></p>
+                <a target='blank' href={ profile.blog }>{ profile.blog }</a>
+                <hr />
+                <h2 className='text-center'>
+                  <span className="label label-primary">
+                    { profile.followers}
+                  </span>
+                  {' '}
+                  <span className="label label-primary">
+                    { profile.following}
+                  </span>
+                </h2>
               </div>
             </div>
           </div>
